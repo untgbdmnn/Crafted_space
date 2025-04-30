4f9e4ea78ba63975950decc3f7232927793848f5
Untung Budiman <untungbudiman.dev@gmail.com>
2025-04-30 19:32:28 +0700

    fix: Adjust layout class for responsive grid display

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function layout({ children }: Props) {
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
-      <div className="w-5/6 min-h-5/6 border p-3 rounded-sm bg-foreground text-background shadow grid grid-cols-9 gap-x-2">
+      <div className="w-5/6 min-h-5/6 border p-3 rounded-sm bg-foreground text-background shadow lg:grid lg:grid-cols-9 flex flex-col gap-x-2">
         <div className="col-span-4 w-full h-full flex items-center justify-center">{children}</div>
         <div className='w-full h-full relative col-span-5 rounded-sm'>
           <Image src={BannerImage} alt='Banner Image' layout='fill' objectFit='cover' className='rounded-sm'/>
